2ef9af40b3da14f7e803af42db09ba50a8b54bbb
Fernando Rocha <fernandorsl@gmail.com>
2016-10-13 00:12:45 -0500

    Trap player in screen bounds

---
+++ Assets/Scripts/Player/PlayerBounds.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBounds : MonoBehaviour {
+
+	private float minX;
+	private float maxX;
+
+	// Use this for initialization
+	void Start () {
+		SetMinAndMaxXBounds ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentPosition = transform.position;
+		if (currentPosition.x < minX) {
+			currentPosition.x = minX;
+		}else if (currentPosition.x > maxX) {
+			currentPosition.x = maxX;
+		}
+
+		transform.position = currentPosition;
+	}
+
+	void SetMinAndMaxXBounds ()
+	{
+		Vector3 bounds = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width, Screen.height, 0));
+		maxX = bounds.x;
+		minX = -bounds.x;
+	}
+}
