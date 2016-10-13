3ec0b5c4cd5627a635a84a638f9d46103cb60004
Fernando Rocha <fernandorsl@gmail.com>
2016-10-12 20:40:18 -0500

    Adding cloud collector and spawner

---
+++ Assets/Scripts/Background/BGScaler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScaler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		//resizing background to fit the camera
+		SpriteRenderer renderer = GetComponent<SpriteRenderer> ();
+
+		//the width of the background sprite
+		float spriteWidth = renderer.bounds.size.x;
+
+		//the height of the ortographic camera
+		float worldHeight = Camera.main.orthographicSize * 2.0f;
+		//the width of the ortographic camera
+		float worldWidth = worldHeight * Camera.main.aspect;
+
+		Vector3 tempScale = transform.localScale;
+		tempScale.x = worldWidth / spriteWidth;
+		transform.localScale = tempScale;
+	}
+
+}
