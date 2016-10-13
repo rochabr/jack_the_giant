34d02d8644f0587a449265d32b91aa05eab178ad
Fernando Rocha <fernandorsl@gmail.com>
2016-10-12 19:43:50 -0500

    Creating player script

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 9.0f;
+	public float maxVelocity = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
